3c30259df9b1c05ec417a9f770a1f6ee2a10fdcf
riyansh100 <riyanshsachdev24@gmail.com>
2026-03-09 17:55:52 +0530

    ohlc postgress insert

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,34 +7,29 @@ import (
 	"os"
 	"time"
 
-	chatredis "github.com/riyansh/chat-backend/internal/redis"
-
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
+
 	"github.com/riyansh/chat-backend/internal/hub"
+	chatredis "github.com/riyansh/chat-backend/internal/redis"
 	"github.com/riyansh/chat-backend/internal/ws"
 )
 
 func main() {
-	// ------------------------------------------------
 	// 1. Instance identity
-	// ------------------------------------------------
 	instanceID := uuid.NewString()
 	log.Println("instanceID:", instanceID)
 
-	// ------------------------------------------------
-	// 2. Redis client (CREATE FIRST)
-	// ------------------------------------------------
+	// 2. Redis
 	rdb := redis.NewClient(&redis.Options{
 		Addr: "localhost:6380",
 	})
 
-	// ------------------------------------------------
-	// 3. Redis in-memory cache (KV)
-	// ------------------------------------------------
+	// 3. Redis cache
 	redisCache := chatredis.NewRedisCache(rdb, 30*time.Second)
 
-	// n. Postgres (TimescaleDB)
+	// 4. Postgres
 	pgConnStr := "postgres://postgres:pwd@localhost:5432/marketdata?sslmode=disable"
 	pool, err := pgxpool.New(context.Background(), pgConnStr)
 	if err != nil {
@@ -43,34 +38,24 @@ func main() {
 	defer pool.Close()
 	log.Println("Postgres connected")
 
-	// ------------------------------------------------
-	// 4. Hub
-	// ------------------------------------------------
-	h := hub.NewHub(instanceID, redisCache, rdb)
+	// 5. Hub
+	h := hub.NewHub(instanceID, redisCache, rdb, pool)
 
 	ctx := context.Background()
 	hub.StartRedisSubscriber(ctx, rdb, h)
-
-	// Start Hub loop
 	go h.Run()
 
-	// ------------------------------------------------
-	// 5. WebSocket handlers
-	// ------------------------------------------------
+	// 6. WebSocket handlers
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		ws.ServeWS(h, w, r)
 	})
-
 	http.HandleFunc("/ws/ingest", ws.IngestHandler(h))
 
-	// ------------------------------------------------
-	// 6. Server
-	// ------------------------------------------------
+	// 7. Server
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-
 	log.Println("Server started on :" + port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
